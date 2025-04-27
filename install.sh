3c0b35843d94323b0ab5e85ffefdf978f1e6241b
glitt3ring <glitt3ring356@gmail.com>
2025-04-27 16:24:41 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
+#!/bin/sh
 sudo mv blockdodgersounds /usr/bin/
 sudo mv blockdodger /usr/bin/
 sudo mv blockdodgerreadme.txt /usr/bin/
-sudo chmod +X /usr/bin/blockdodger
+sudo chmod +x /usr/bin/blockdodger
+sudo chmod +x /usr/bin/blockdodgerreadme
